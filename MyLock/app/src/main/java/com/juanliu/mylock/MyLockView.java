0a607dbe0afe839b072a14df34d71eae56d66cd4
iamlira <474948760@qq.com>
2015-07-15 11:49:29 +0800

    add some code,but its wrong

--- MyLock/app/src/main/java/com/juanliu/mylock/MyLockView.java
+++ MyLock/app/src/main/java/com/juanliu/mylock/MyLockView.java
@@ -7,6 +7,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -15,11 +17,14 @@ import android.view.SurfaceView;
  */
 public class MyLockView extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder holder;
-    private int width, height;
+    private boolean draw = false;
+    private int width, height, stick_x, stick_y;
     private Paint_Thread paint_thread;
+    private Point[][] list = new Point[3][3];
 
     public MyLockView(Context context) {
         super(context);
+        setFocusable(true);
         holder = getHolder();
         holder.addCallback(this);
         paint_thread = new Paint_Thread();
@@ -27,6 +32,7 @@ public class MyLockView extends SurfaceView implements SurfaceHolder.Callback {
 
     public MyLockView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setFocusable(true);
         holder = getHolder();
         holder.addCallback(this);
         paint_thread = new Paint_Thread();
@@ -34,6 +40,7 @@ public class MyLockView extends SurfaceView implements SurfaceHolder.Callback {
 
     public MyLockView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setFocusable(true);
         holder = getHolder();
         holder.addCallback(this);
         paint_thread = new Paint_Thread();
@@ -41,7 +48,10 @@ public class MyLockView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        paint_thread.start();
+        width = this.getWidth();
+        height = this.getHeight();
+//        paint_thread.start();
+        init_draw();
     }
 
     @Override
@@ -66,7 +76,6 @@ public class MyLockView extends SurfaceView implements SurfaceHolder.Callback {
             height = getHeight();
             Paint paint = new Paint();
             paint.setColor(Color.RED);
-
             bt = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
             for (int i = 1; i <= 3; i++)
                 for (int j = 1; j <= 3; j++) {
@@ -74,9 +83,97 @@ public class MyLockView extends SurfaceView implements SurfaceHolder.Callback {
                 }
 
 
-            if(canvas!=null){
+            if (canvas != null) {
                 holder.unlockCanvasAndPost(canvas);
             }
+        }
+    }
+    public void clear(){
+        Canvas canvas = holder.lockCanvas();
+        canvas.drawColor(Color.WHITE);
+        Paint paint = new Paint();
+        Bitmap bt = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        for (int i = 1; i <= 3; i++)
+            for (int j = 1; j <= 3; j++) {
+                canvas.drawBitmap(bt, i * width / 4 - bt.getWidth()/2, j * height / 4 - bt.getHeight()/2, paint);
+            }
+
+        if (canvas != null) {
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void init_draw() {
+        Canvas canvas = holder.lockCanvas();
+        canvas.drawColor(Color.WHITE);
+        Paint paint = new Paint();
+        Bitmap bt = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        for (int i = 1; i <= 3; i++)
+            for (int j = 1; j <= 3; j++) {
+                canvas.drawBitmap(bt, i * width / 4 - bt.getWidth()/2, j * height / 4 - bt.getHeight()/2, paint);
+                list[i - 1][j - 1] = new Point(i * width / 4, j * height / 4);
+            }
+
+        if (canvas != null) {
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void move_draw(float x, float y) {
+        Canvas canvas = holder.lockCanvas();
+        canvas.drawColor(Color.WHITE);
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        Bitmap bt = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        for (int i = 1; i <= 3; i++)
+            for (int j = 1; j <= 3; j++) {
+                canvas.drawBitmap(bt, i * width / 4 - bt.getWidth()/2, j * height / 4 - bt.getHeight()/2, paint);
+            }
+        canvas.drawLine(stick_x, stick_y, x, y, paint);
+        if (canvas != null) {
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void validate(int x, int y) {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                if (x <= list[i][j].x + 40 && x >= list[i][j].x - 40 && y <= list[i][j].y + 40 && y >= list[i][j].y + 40) {
+                    Log.d("ff",list[i][j].x+"  "+list[i][j].y);
+                    stick_x = list[i][j].x;
+                    stick_y = list[i][j].y;
+                    draw = true;
+                }
+
+            }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                validate((int) event.getX(), (int) event.getY());
+                Log.d("look",(int) event.getX()+"  "+ (int) event.getY()+"  " +draw);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if(draw)
+                move_draw(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+//                clear();
+//                draw = false;
+                break;
+        }
+        return true;
+    }
+
+
+    class Point {
+        int x, y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
